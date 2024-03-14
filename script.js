095ef77f687793ee2e9e6acb36b7c55bd42cd837
clownstv <163416367+clownstv@users.noreply.github.com>
2024-03-14 15:47:26 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let clowns = [
+    {
+        name:"r99",
+        pic:"./images/enemy.png",
+        movie:"../movies/r99.mp4",
+        page:"./pages/r99.html"
+    },
+
+]
+let a = document.getElementById("main-center")
+clowns.forEach(e => {
+    a.innerHTML += `<div class='clowns'> <img width='250px' height='250px' src='${e.pic}' alt='Dalghak1'></img><br><span><a href='${e.page}'>${e.name}</a></span></div>`;
+});
