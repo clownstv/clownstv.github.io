e7b28fa7be82c2b84045f9d08157c222a4251319
clownstv <163416367+clownstv@users.noreply.github.com>
2024-03-20 00:15:11 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let clowns = [
     {
-        name:"Stalker و دوستان",
+        name:"استاکر و دوستان",
         pic:"./images/enemy.png",
         movie:"../movies/r99.mp4",
         page:"./pages/r99.html"
@@ -9,5 +9,5 @@ let clowns = [
 ]
 let a = document.getElementById("main-center")
 clowns.forEach(e => {
-    a.innerHTML += `<div class='clowns'> <img width='250px' height='250px' src='${e.pic}' alt='Dalghak1'></img><br><span><a href='${e.page}'>${e.name}</a></span></div>`;
+    a.innerHTML += `<div dir="rtl" class='clowns'> <img width='250px' height='250px' src='${e.pic}' alt='Dalghak1'></img><br><span><a href='${e.page}'>${e.name}</a></span></div>`;
 });
